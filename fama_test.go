e69cf682d93e2dec92e26cf10515e78504033bff
MathisWellmann <wellmannmathis@gmail.com>
2020-03-10 15:12:47 +0100

    fixed mama. added TestFAMAGraph

--- fama_test.go
+++ fama_test.go
@@ -1,7 +1,17 @@
 package EhlersTAIndicators
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestFAMA(t *testing.T) {
+func TestFAMAGraph(t *testing.T) {
+	vals := GenerateTimeseries(1024)
 
+	fama := FAMADefault(vals)
+	filename := fmt.Sprintf("img/fama.png")
+	err := Plt(fama, filename)
+	if err != nil {
+		t.Error(err)
+	}
 }
--- mama.go
+++ mama.go
@@ -16,6 +16,7 @@ func MAMAFAMA(vals []float64, fastLimit, slowLimit float64, which int) []float64
 	period := make([]float64, len(vals))
 	detrender := make([]float64, len(vals))
 	is := make([]float64, len(vals))
+	i2s := make([]float64, len(vals))
 	qs := make([]float64, len(vals))
 	q2s := make([]float64, len(vals))
 	res := make([]float64, len(vals))
@@ -26,39 +27,39 @@ func MAMAFAMA(vals []float64, fastLimit, slowLimit float64, which int) []float64
 	fama := make([]float64, len(vals))
 
 	for i := 0; i < len(vals); i++ {
-		if i < 5 {
+		if i <= 5 {
 			mama[i] = vals[i]
 			fama[i] = vals[i]
+			continue
 		}
-		s := (4*vals[i] + 3*vals[i-1] + 2*vals[i-2] + vals[i-3]) / 10
-		smooth[i] = s
-		d := (0.0962*s + 0.5769*smooth[i-2] - 0.5769*smooth[i-4] - 0.0962*smooth[i-6]) * (0.075*period[i-1] + 0.54)
-		detrender[i] = d
+		smooth[i] = (4*vals[i] + 3*vals[i-1] + 2*vals[i-2] + vals[i-3]) / 10
+		detrender[i] = (0.0962*smooth[i] + 0.5769*smooth[i-2] - 0.5769*smooth[i-4] - 0.0962*smooth[i-6]) * (0.075*period[i-1] + 0.54)
 
 		// compute InPhase and Quadrature components
 		q1 := (0.0962*detrender[i] + 0.5769*detrender[i-2] - 0.5769*detrender[i-4] - 0.0962*detrender[i-6]) * (0.075*period[i-1] + 0.54)
 		qs[i] = q1
+		is[i] = detrender[i-3]
 
 		// Advance the phase of detrender and q1 by 90 Degrees
-		jI := (0.0962*detrender[i-3] + 0.05769*detrender[i-5] - 0.5769*detrender[i-7] - 0.0962*detrender[i-9]) * (0.075*period[i-1] + 0.54)
+		jI := (0.0962*is[i] + 0.05769*is[i-2] - 0.5769*is[i-4] - 0.0962*is[i-6]) * (0.075*period[i-1] + 0.54)
 		jQ := (0.0962*qs[i] + 0.5769*qs[i-2] - 0.5769*qs[i-4] - 0.0962*qs[i-6]) * (0.075*period[i-1] + 0.54)
 
 		// Phasor addition for 3 bar averaging
 		i2 := detrender[i-3] - jQ
-		is[i] = i2
+		i2s[i] = i2
 		q2 := q1 + jI
 		q2s[i] = q2
 
 		// smooth the I and Q components befor applying the discriminator
-		i2 = 0.2*i2 + 0.8*is[i-1]
-		is[i] = i2
+		i2 = 0.2*i2 + 0.8*i2s[i-1]
+		i2s[i] = i2
 		q2 = 0.2*q2 + 0.8*qs[i-1]
 		q2s[i] = q2
 
 		// Homodyne Discriminator
-		re := i2*is[i-1] + q2*q2s[i-1]
+		re := i2*i2s[i-1] + q2*q2s[i-1]
 		res[i] = re
-		im := i2*q2s[i-1] - q2*is[i-1]
+		im := i2*q2s[i-1] - q2*i2s[i-1]
 		ims[i] = im
 		re = 0.2*re + 0.8*res[i-1]
 		im = 0.2*im + 0.8*ims[i-1]
